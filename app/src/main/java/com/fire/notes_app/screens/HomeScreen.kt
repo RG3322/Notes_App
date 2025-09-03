8c5a09d7559c2eaf82fa1ff27d7b2b157a6feeea
RG3322 <raghavsankhyaan@gmail.com>
2025-09-03 17:13:52 +0530

    added some dependencies

---
+++ app/src/main/java/com/fire/notes_app/screens/HomeScreen.kt
@@ -0,0 +1,94 @@
+package com.fire.notes_app.screens
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fire.notes_app.R
+
+data class Note(
+    val title: String,
+    val description: String
+)
+@Preview(showBackground = true)
+@Composable
+fun HomeScreen() {
+    val notesList = List(7) {
+        Note(
+            title = "Test 2 - 1234",
+            description = "another day"
+        )
+    }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* TODO: Add Note Action */ },
+                containerColor = MaterialTheme.colorScheme.tertiary
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .background(Color.LightGray),
+            verticalArrangement = Arrangement.spacedBy(14.dp),
+            contentPadding = PaddingValues(vertical = 18.dp)
+        ) {
+            items(notesList) { note ->
+                NoteCard(note)
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteCard(note: Note) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp)
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.titleMedium,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = note.description,
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.DarkGray
+            )
+        }
+    }
+}
+
+/**
+ *
+ */
